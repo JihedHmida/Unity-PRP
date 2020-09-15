0455645cbb4ff1bced8683027c9e862575b5b317
JihedHmida <Jihed.hmida@gmail.com>
2020-09-15 17:59:46 +0100

    GameClosedDuration

---
+++ Assets/Editor/PlayerPrefsRemover.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class PlayerPrefsRemover : EditorWindow
+{
+
+    [MenuItem("Tools/Delete Player Prefs")]
+    public static void DeletePlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+        Debug.Log("Player Prefs Deleted ! ");
+    }
+}
---
+++ Assets/Reusable/GameClosedDuration/PlayerSession.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerSession : MonoBehaviour
+{
+
+    [SerializeField] GameObject welcomePopup;
+    [SerializeField] TextMeshProUGUI welcomePopupDuration;
+    public static PlayerSession I { get; private set; }
+
+    void Awake()
+    {
+        if (I == null)
+        {
+            I = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Start()
+    {
+        //welcomePopupShow();
+        Debug.Log("app Started");
+        string dateQuitString = PlayerPrefs.GetString("dateQuit", "");
+        if (!dateQuitString.Equals(""))
+        {
+            DateTime dateQuit = DateTime.Parse(dateQuitString);
+            DateTime dateNow = DateTime.Now;
+            if (dateNow > dateQuit)
+            {
+                TimeSpan timeSpan = dateNow - dateQuit;
+
+                //TODO Change to Preferred Duration 
+                int seconds = (int)timeSpan.TotalSeconds;
+                Debug.Log("qui for :" + seconds + "seconds");
+                welcomePopupDuration.text = seconds + " SECONDS";
+
+            }
+            PlayerPrefs.SetString("dateQuit", "");
+
+        }
+
+    }
+
+    private void OnApplicationQuit()
+    {
+        DateTime dateQuit = DateTime.Now;
+        PlayerPrefs.SetString("dateQuit", dateQuit.ToString());
+        Debug.Log("quit at :" + dateQuit.ToString());
+    }
+
+    private void welcomePopupShow()
+    {
+        welcomePopup.SetActive(true);
+    }
+    public void welcomePopupHide()
+    {
+        welcomePopup.SetActive(false);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
