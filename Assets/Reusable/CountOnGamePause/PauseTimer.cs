9db131b4b02e3186e69f5e8bbfabe558e916ae95
JihedHmida <Jihed.hmida@gmail.com>
2020-09-16 17:12:07 +0100

    CountOnGamePause

---
+++ Assets/Reusable/CountOnGamePause/PauseTimer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+using TMPro;
+public class PauseTimer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI conterText;
+    int counter = 1;
+    bool isStarted = false;
+    DateTime pauseDateTime;
+    void Start()
+    {
+        isStarted = true;
+        StartCoroutine(Count());
+    }
+
+    private void OnApplicationPause(bool isPaused)
+    {
+        if (isStarted)
+        {
+            if (isPaused)
+            {
+                pauseDateTime = DateTime.Now;
+            }
+            else
+            {
+                counter = (int)(DateTime.Now - pauseDateTime).TotalSeconds;
+            }
+
+        }
+    }
+
+    IEnumerator Count()
+    {
+        while (true)
+        {
+            conterText.text = (counter++).ToString();
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
